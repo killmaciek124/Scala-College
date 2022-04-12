c8144a200ec233a7885c09d3f4d143508a24d36d
killmaciek124 <maciej@maziuk.com.pl>
2022-04-12 09:40:53 +0200

    do 03 lab

--- oklab02/src/main/scala/Main.scala
+++ oklab02/src/main/scala/Main.scala
@@ -1,11 +1,16 @@
-def parzysta(n: Int): Boolean = {
+// LAB02 DONE !!!
+
+import scala.util.control.Breaks._
+  def parzysta(n: Int): Boolean = {
     n % 2 == 0
 }
-
 @main
 def zadanie_02: Unit = {
-    println(s"parzysta(5) == ${parzysta(5)}")
+    println(s"parzysta(6) == ${parzysta(6)}")
 }
+
+
+
 
 
 def nwd(A: Int, B: Int): Int = {
@@ -18,25 +23,49 @@ def nwd(A: Int, B: Int): Int = {
         b -=a
       }
     }
-    return a 
+     a 
 }
 
 @main
 def zadanie_03: Unit = {
-    println(s"nwd(10, 30) == ${nwd(100,25)}")
+    println(s"nwd(30,100) == ${nwd(30,100)}")
 }
 
 def pierwsza(n: Int): Boolean = {
     assert(n >= 2)
     for (i <- 2 to (n-1)) {
-    if (n % i == 0) {
-      false
-    }
+    println(s"$i, $n")
+    if (n % i == 0) return false;
 }
-true
+    return  true;
 }
 
 @main
 def zadanie_04(liczba: Int): Unit = {
-    println(s"pierwsza(15) == ${pierwsza(15)}")
+    println(s"pierwsza($liczba) == ${pierwsza(liczba)}")
+}
+
+def hipoteza(n: Int): Unit = {
+    breakable {
+    for (i <- 2 to (n/2)) {
+      if (pierwsza(i) == true) {
+        if (pierwsza(n-i) == true) {
+          val result = (n-i)
+          println(s"wynik : $result, $i")
+          break
+        }
+      }
+    }
+    print("Nie ma liczb takich!")
+  }
+}
+
+@main
+def zadanie_05(n: Int): Unit = {
+    assert(n >2)
+    assert(n%2==0)   
+    hipoteza(n)
+    
+    //   TAK -> wywołujemy hipoteza(n)
+    //   NIE -> kończymy z odpowiednim komunikatem.
 }
